38cb1e48d682da30aa73f9c1ccd1121bb21778d5
Alexpr0100 <89502981177@mail.ru>
2023-06-22 11:50:17 +1000

    String

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+
+
+string FindElements(string [] array) 
+{
+    string result = ""; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) result += array[i] + " ";
+    }
+    return result;
+}
+
+string [] ChangeStringToArray(string newstring)
+{
+    return newstring.Split(' ', StringSplitOptions.RemoveEmptyEntries);  
+}
+
+
