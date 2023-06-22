c2dbabb5218b152e14d21baaf8506006d97005a8
Alexpr0100 <89502981177@mail.ru>
2023-06-22 11:51:01 +1000

    void

--- Program.cs
+++ Program.cs
@@ -17,4 +17,17 @@ string [] ChangeStringToArray(string newstring)
     return newstring.Split(' ', StringSplitOptions.RemoveEmptyEntries);  
 }
 
+void PrintArray(string[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
 
+string [] FirstArray = {"hello", "22", "great", "hey"};
+string elements = FindElements(FirstArray);
+string [] resultArray = ChangeStringToArray(elements);
+System.Console.Write($"Elements of array with length <= 3: ");
+PrintArray(resultArray);
